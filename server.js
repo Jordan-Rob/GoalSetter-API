6b553b54ea62fa5bccbc42a9334607796e9e2e51
Jordan <jordanrob709@gmail.com>
2022-06-13 17:14:55 +0300

    added mongodb connection

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const goalsRouter = require('./routes/goalRoutes')
+const colors = require('colors')
+const connectDB = require('./config/db')
 
 const port = process.env.PORT || 5000
+
+connectDB()
+
 const app = express()
 
 app.use(express.json())
